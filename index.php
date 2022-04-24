434f3464a1fe203f48950239664e5dd485d3d563
JasonPsilva <104292857+JasonPsilva@users.noreply.github.com>
2022-04-24 05:08:13 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,54 +1,27 @@
-<?php
-
-
-
-if(isset($_POST['submit'])){
-
-    include_once('config.php');
-
-    $porteiro = $_POST['porteiro'];
-    $data = $_POST['data_dia'];
-    $ocorrencias = $_POST['ocorrencias'];
-
-    $result =mysqli_query($conexao, "INSERT INTO livro (porteiro, data,ocorrencias) 
-    VALUES('$porteiro','$data', '$ocorrencias')");
-
-
-
-}
-
-
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>site.php </title>
+    <title>Inscrição natação</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
-
 <body>
-    <br><br>
-    <form action="index.php" method="POST">
-        <h1>PORTEIRO</h1> 
-         <input type="Porteiro" name="porteiro"><br><br>
-        <h2>DATA</h2> 
-        <input type="date" name="data_dia"><br><br>
-       <h2> OCORÊNCIAS</h2>
-        <input type="text" name="ocorrencias" class="ocorrencias"><br><br><br>
-        <input type="submit" value="Enviar" name="submit" class="botao">
 
+<h2>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</h2><BR>
+
+   <form action="script.php" method="POST">
+    <p>SEU NOME:</p>
+    <input type="text" name="seuNome" max="30"><br><br>
+    <p>SUA IDADE:</p>
+    <input type="text" name="suaIdade" max="3"><br><br><br>
+
+    <input type="submit" value="INSCREVER">
+    
+    
 
 
     </form>
-    <footer >
-        SITE CRIADO POR JASON P. SILVA
-    </footer>
-   
 </body>
 </html>
